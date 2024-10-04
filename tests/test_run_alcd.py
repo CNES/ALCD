f5f4dfdc078918ad01c068007ce97ecd5ee91fb2
Arthur VINCENT <arthur.vincent@csgroup.eu>
2024-10-04 10:01:08 +0200

    test: add tests docstrings

--- tests/test_run_alcd.py
+++ tests/test_run_alcd.py
@@ -1,9 +1,29 @@
 import json
 import shutil
 import subprocess
+from pathlib import Path
+from typing import TYPE_CHECKING
 
-def prepare_test_dir(alcd_paths, output_dir):
+if TYPE_CHECKING:
+    from tests.conftest import ALCDTestsData
+
+def prepare_test_dir(alcd_paths: ALCDTestsData, output_dir) -> tuple[Path, Path]:
     """
+    Prepares the test directory by copying reference data and updating configuration files.
+
+    Parameters
+    ----------
+    alcd_paths : ALCDTestsData
+        An object containing the paths for the reference run, configuration files,
+        and data directories.
+    output_dir : Path
+        The directory where the test output will be saved.
+
+    Returns
+    -------
+    tuple[Path, Path]
+        A tuple containing the paths to the modified `global_parameters.json` and
+        `paths_configuration.json` files.
     """
 
     shutil.copytree(alcd_paths.reference_run, output_dir, dirs_exist_ok=True)
@@ -25,22 +45,47 @@ def prepare_test_dir(alcd_paths, output_dir):
         parameters_file.write(json.dumps(paths_parameters, indent=3, sort_keys=True))
     return out_global_parameters, out_path_parameters
 
-def test_run_alcd(alcd_paths):
+def test_run_alcd(alcd_paths: ALCDTestsData) -> None:
+    """
+    Tests the execution of the ALCD pipeline by running the main ALCD
+    script with specific parameters.
 
+    Parameters
+    ----------
+    alcd_paths : ALCDTestsData
+        An object containing paths related to the project, such as configuration
+        and data directories.
+
+    Raises
+    ------
+    AssertionError
+        If the ALCD process fails (i.e., returns a non-zero exit code).
+    """
     global_param_file, paths_param_file = prepare_test_dir(alcd_paths, alcd_paths.data_dir / "test_run_alcd" / "Toulouse_31TCJ_20240305")
 
     cmd = f"python {alcd_paths.project_dir}/all_run_alcd.py -f True -s 1 -l Toulouse -d 20240305 -c 20240120 -dates False -kfold False -force False -global_parameters {global_param_file} -paths_parameters {paths_param_file} -model_parameters {alcd_paths.cfg}/model_parameters.json"
-    # subprocess.run(cmd.split(), capture_output=True, text=True)
     proc = subprocess.Popen(
         cmd, shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT
     )
-    # Get output as strings
-    out, err = proc.communicate()
+    out, _ = proc.communicate()
     assert proc.returncode == 0, out.decode('utf-8')
 
 
-def test_run_alcd_gen_features(alcd_paths):
+def test_run_alcd_gen_features(alcd_paths: ALCDTestsData) -> None:
+    """
+    Tests the feature generation stage of the ALCD pipeline by running the appropriate ALCD script.
 
+    Parameters
+    ----------
+    alcd_paths : ALCDTestsData
+        An object containing paths related to the project, such as configuration
+        and data directories.
+
+    Raises
+    ------
+    AssertionError
+        If the ALCD process fails (i.e., returns a non-zero exit code).
+    """
     global_param_file, paths_param_file = prepare_test_dir(alcd_paths,
                                                            alcd_paths.data_dir / "test_gen_features" / "Toulouse_31TCJ_20240305")
 
@@ -49,18 +94,26 @@ def test_run_alcd_gen_features(alcd_paths):
     proc = subprocess.Popen(
         cmd, shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT
     )
-
-    # Get output as strings
-    out, err = proc.communicate()
+    out, _ = proc.communicate()
     assert proc.returncode == 0, out.decode('utf-8')
 
-def test_quicklook(alcd_paths):
+def test_quicklook(alcd_paths:ALCDTestsData)->None:
+    """
+    Tests the generation of quicklook images using the `quicklook_generator.py` script.
+
+    Parameters
+    ----------
+    alcd_paths : ALCDTestsData
+        An object containing paths related to the project, such as configuration directories.
+
+    Raises
+    ------
+    AssertionError
+        If the quicklook generation process fails (i.e., returns a non-zero exit code).
+    """
     cmd = f"python {alcd_paths.project_dir}/quicklook_generator.py -l Toulouse -paths_parameters {alcd_paths.cfg}/paths_configuration.json"
-    # subprocess.run(cmd.split(), capture_output=True, text=True)
     proc = subprocess.Popen(
         cmd, shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT
     )
-
-    # Get output as strings
-    out, err = proc.communicate()
+    out, _ = proc.communicate()
     assert proc.returncode == 0, out.decode('utf-8')
