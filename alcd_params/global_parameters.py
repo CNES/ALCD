c06df47a7ae558508cf9fc6f6d5893716cd469b4
Arthur VINCENT <arthur.vincent@csgroup.eu>
2024-10-29 16:45:05 +0100

    feat: start adding pydantic BaseModel

---
+++ alcd_params/global_parameters.py
@@ -0,0 +1,253 @@
+from typing import Dict, List
+
+from pydantic import BaseModel, FilePath, field_validator, ValidationInfo
+from datetime import datetime
+
+
+class Classification(BaseModel):
+    """
+    Classification configuration for ALCD.
+
+    Attributes
+    ----------
+    method : str
+        The method of classification to be used, e.g., "rf" for random forest.
+    """
+    method: str
+
+
+class Features(BaseModel):
+    """
+    Configuration for feature extraction in ALCD.
+
+    Attributes
+    ----------
+    DTM : str
+        Indicator for whether a Digital Terrain Model (DTM) is used.
+    original_bands : List[int]
+        List of original spectral bands to include in the feature extraction.
+    ratios : List[str]
+        Ratios between bands, typically in the format "bandX_bandY".
+    special_indices : List[str]
+        List of special indices to calculate, e.g., NDVI, NDWI.
+    textures : bool
+        Whether or not to include texture analysis in feature extraction.
+    time_difference_bands : List[int]
+        List of bands for which time-difference calculations will be performed.
+    """
+    DTM: str
+    original_bands: List[int]
+    ratios: List[str]
+    special_indices: List[str]
+    textures: bool
+    time_difference_bands: List[int]
+
+
+class General(BaseModel):
+    """
+    General configuration related to ALCD's internal operations.
+
+    Attributes
+    ----------
+    class_stats : str
+        File name for storing class statistics.
+    img_labeled : str
+        File name for the labeled image output.
+    img_labeled_regularized : str
+        File name for the regularized labeled image output.
+    img_stats : str
+        File name for image statistics.
+    merged_layers : str
+        File name for storing merged layer shapefile.
+    no_data_mask : str
+        File name for no-data mask shapefile.
+    training_samples_extracted : str
+        File name for the extracted training samples.
+    training_samples_location : str
+        File name for the location of training samples.
+    training_sampling : str
+        Sampling strategy for training data, e.g., "smallest".
+    training_shp : str
+        File name for the training shapefile.
+    training_shp_extended : str
+        File name for the extended training shapefile.
+    validation_shp : str
+        File name for the validation shapefile.
+    validation_shp_extended : str
+        File name for the extended validation shapefile.
+    """
+    class_stats: str
+    img_labeled: str
+    img_labeled_regularized: str
+    img_stats: str
+    merged_layers: str
+    no_data_mask: str
+    training_samples_extracted: str
+    training_samples_location: str
+    training_sampling: str
+    training_shp: str
+    training_shp_extended: str
+    validation_shp: str
+    validation_shp_extended: str
+
+
+class LocalPaths(BaseModel):
+    """
+    Paths related to local storage and server configuration.
+
+    Attributes
+    ----------
+    copy_folder : str
+        Path for the folder where data copies are stored.
+    current_server : str
+        Current server address for data transfer, e.g., "user@server:/path".
+    """
+    copy_folder: str
+    current_server: str
+
+
+class Mask(BaseModel):
+    """
+    Configuration for masks in ALCD.
+
+    Attributes
+    ----------
+    class_name : int
+        Identifier for the class associated with the mask.
+    shp : str
+        File name for the mask shapefile.
+    """
+    class_name: int
+    shp: str
+
+
+class PostProcessing(BaseModel):
+    """
+    Configuration for post-processing steps in ALCD.
+
+    Attributes
+    ----------
+    binary_confusion_matrix : str
+        File name for the binary confusion matrix CSV file.
+    confusion_matrix : str
+        File name for the overall confusion matrix CSV file.
+    model_metrics : str
+        File name for model performance metrics CSV file.
+    """
+    binary_confusion_matrix: str
+    confusion_matrix: str
+    model_metrics: str
+
+
+class TrainingParameters(BaseModel):
+    """
+    Parameters for training models in ALCD.
+
+    Attributes
+    ----------
+    Kfold : int
+        Number of folds for K-fold cross-validation.
+    dilatation_radius : int
+        Radius for dilating the training samples.
+    expansion_distance : int
+        Distance for expanding the sample areas.
+    regularization_radius : int
+        Radius for regularization.
+    training_proportion : float
+        Proportion of the dataset to be used for training.
+    """
+    Kfold: int
+    dilatation_radius: int
+    expansion_distance: int
+    regularization_radius: int
+    training_proportion: float
+
+
+class UserChoices(BaseModel):
+    """
+    User-defined settings for the ALCD process.
+
+    Attributes
+    ----------
+    clear_date : str
+        Date in YYYYMMDD format for a clear-sky image.
+    current_date : str
+        Current processing date in YYYYMMDD format.
+    location : str
+        Name of the location being analyzed.
+    main_dir : str
+        Main directory for data processing and storage.
+    raw_img : str
+        File path for the raw input image.
+    tile : str
+        Tile identifier for the region of interest.
+    """
+    clear_date: str
+    current_date: str
+    location: str
+    main_dir: str
+    raw_img: str
+    tile: str
+
+    @field_validator("clear_date", "current_date")
+    def parse_yyyymmdd(cls, value: str, info: ValidationInfo) -> str:
+        """
+        Validates the date format YYYYMMDD and checks if the date is valid.
+
+        Parameters
+        ----------
+        value : str
+            Date string to validate.
+        info : ValidationInfo
+            Information about the field being validated.
+
+        Returns
+        -------
+        str
+            Validated date string.
+
+        Raises
+        ------
+        ValueError
+            If the date is not in the correct format or is not a valid date.
+        """
+        if isinstance(value, str) and len(value) == 8:
+            try:
+                return str(datetime.strptime(value, "%Y%m%d").date())
+            except ValueError:
+                raise ValueError("dates must be in YYYYMMDD format and a valid date.")
+        else:
+            raise ValueError("dates must be in YYYYMMDD format.")
+
+
+class ALCDConfig(BaseModel):
+    """
+    Main configuration model for the ALCD project.
+
+    Attributes
+    ----------
+    classification : Classification
+        Configuration for the classification method used.
+    color_tables : Dict[str, FilePath]
+        Dictionary of paths for color tables, with keys representing table names.
+    features : Features
+        Feature extraction settings.
+    general : General
+        General operational configurations.
+    masks : Dict[str, Mask]
+        Dictionary of mask configurations, with mask names as keys.
+    postprocessing : PostProcessing
+        Settings for post-processing outputs and metrics.
+    training_parameters : TrainingParameters
+        Training configuration and parameters.
+    user_choices : UserChoices
+        User-specified configurations and metadata.
+    """
+    classification: Classification
+    color_tables: Dict[str, FilePath]
+    features: Features
+    general: General
+    masks: Dict[str, Mask]
+    postprocessing: PostProcessing
+    training_parameters: TrainingParameters
+    user_choices: UserChoices
---
+++ alcd_params/models_parameters.py
@@ -0,0 +1,215 @@
+from PyQt5.uic.Compiler.misc import Literal
+from pydantic import BaseModel
+
+
+class LibSVMConfig(BaseModel):
+    """
+    Configuration parameters for the LibSVM model.
+
+    Attributes
+    ----------
+    k : str
+        Kernel type for the SVM (e.g., "linear").
+    m : str
+        Model type for the SVM (e.g., "csvc").
+    c : str
+        Cost parameter for the SVM (e.g., "1").
+    opt : str
+        Optimization flag, specifying whether optimization is enabled ("true"/"false").
+    prob : str
+        Probability estimates flag ("true"/"false").
+    """
+    k: Literal["linear", "rbf", "poly", "sigmoid"]
+    m: Literal["csvc", "nusvc", "oneclass"]
+    c: int
+    opt: str
+    prob: str
+
+
+class BoostConfig(BaseModel):
+    """
+    Configuration parameters for the Boost model.
+
+    Attributes
+    ----------
+    t : str
+        Boosting type (e.g., "real").
+    w : str
+        Weak count.
+    r : str
+        Weight Trim Rate.
+    m : str
+        Maximum depth of the boost model.
+    """
+    t: Literal["discrete", "real", "logit", "gentle"]
+    w: int
+    r: float
+    m: int
+
+
+class DTConfig(BaseModel):
+    """
+    Configuration parameters for the Decision Tree (DT) model.
+
+    Attributes
+    ----------
+    max : str
+        Maximum depth of the decision tree.
+    min : str
+        Minimum samples required to split an internal node.
+    ra : str
+        Regularization amount.
+    cat : str
+        Number of categories in categorical features.
+    f : str
+        Maximum number of features to consider for splits.
+    r : str
+        Flag indicating randomization ("true"/"false").
+    t : str
+        Flag for additional tree configuration ("true"/"false").
+    """
+    max: int
+    min: int
+    ra: float
+    cat: int
+    f: int
+    r: str
+    t: str
+
+
+class GBTConfig(BaseModel):
+    """
+    Configuration parameters for the Gradient Boosted Trees (GBT) model.
+
+    Attributes
+    ----------
+    w : str
+        Number of boosting iterations.
+    s : str
+        Regularization parameter
+    p : str
+        Portion of the whole training set used for each algorithm iteration
+    max : str
+        Maximum depth of the tree.
+    """
+    w: str
+    s: str
+    p: str
+    max: str
+
+
+class ANNConfig(BaseModel):
+    """
+    Configuration parameters for the Artificial Neural Network (ANN) model.
+
+    Attributes
+    ----------
+    t : str
+        Type of neural network (e.g., "reg" for regression).
+    sizes : int
+        Size of the hidden layers.
+    f : str
+        Activation function (e.g., "sig" for sigmoid).
+    a : float
+        Alpha regularization parameter.
+    b : str
+        Beta regularization parameter.
+    bpdw : str
+        Strength of the weight gradient term in the BACKPROP method
+    bpms : str
+        Strength of the momentum term (the difference between weights on the 2 previous iterations)
+    rdw : float
+        Initial value Delta_0 of update-values Delta_{ij} in RPROP method
+    rdwm : str
+        Update-values lower limit Delta_{min} in RPROP method.
+    term : str
+        Termination criteria [iter/eps/all].
+    eps : float
+        Epsilon value used in the Termination criteria.
+    iter : int
+        Maximum number of iterations used in the Termination criteria.
+    """
+    t: Literal["back", "reg"]
+    sizes: int
+    f: Literal["ident", "sig", "gau"]
+    a: float
+    b: float
+    bpdw: float
+    bpms: float
+    rdw: float
+    rdwm: float
+    term: Literal["iter", "eps", "all"]
+    eps: float
+    iter: int
+
+
+class RFConfig(BaseModel):
+    """
+    Configuration parameters for the Random Forest (RF) model.
+
+    Attributes
+    ----------
+    max : int
+        Maximum depth of the forest.
+    min : int
+        Minimum number of samples in each node.
+    ra : float
+        Termination Criteria for regression tree.
+    cat : int
+        Cluster possible values of a categorical variable into K <= cat clusters to find a suboptimal split.
+    var : int
+        Size of the randomly selected subset of features at each tree node.
+    nbtrees : int
+        Maximum number of trees in the forest.
+    acc : float
+        Sufficient accuracy (OOB error)
+    """
+    max: int
+    min: str
+    ra: str
+    cat: str
+    var: str
+    nbtrees: str
+    acc: str
+
+
+class KNNConfig(BaseModel):
+    """
+    Configuration parameters for the K-Nearest Neighbors (KNN) model.
+
+    Attributes
+    ----------
+    k : str
+        Number of neighbors to consider.
+    """
+    k: str
+
+
+class MLConfig(BaseModel):
+    """
+    Configuration for multiple machine learning models.
+
+    Attributes
+    ----------
+    libsvm : LibSVMConfig
+        Configuration for the LibSVM model.
+    boost : BoostConfig
+        Configuration for the Boost model.
+    dt : DTConfig
+        Configuration for the Decision Tree model.
+    gbt : GBTConfig
+        Configuration for the Gradient Boosted Trees model.
+    ann : ANNConfig
+        Configuration for the Artificial Neural Network model.
+    rf : RFConfig
+        Configuration for the Random Forest model.
+    knn : KNNConfig
+        Configuration for the K-Nearest Neighbors model.
+    """
+    libsvm: LibSVMConfig
+    boost: BoostConfig
+    dt: DTConfig
+    gbt: GBTConfig
+    ann: ANNConfig
+    rf: RFConfig
+    knn: KNNConfig
